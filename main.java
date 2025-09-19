08c55f7529703a35824c1d0db401515c288ea34f
DrewGlover05 <drewglover05@icloud.com>
2025-09-18 20:52:32 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,16 +1,18 @@
 public class main {
     public static void main(String[] args) {
+        // Creates the objects BrandAFactory & BrandBFactory
         SmartDeviceFactory brandAFactory = new BrandAFactory();
         SmartDeviceFactory brandBFactory = new BrandBFactory();
 
+        // Creates the objects BrandABulb & BrandBLock
         SmartBulb brandABulb = brandAFactory.createBulb();
-
         SmartLock brandBLock = brandBFactory.createLock();
 
+        // displays both of the usages by calling the method displayUsage();
         System.out.println("Brand A Smart Bulb Usage:");
         brandABulb.displayUsage();
-    
         System.out.println("Brand B Smart Lock Usage:");
         brandBLock.displayUsage();
     }
+
 }
