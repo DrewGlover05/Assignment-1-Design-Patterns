b1557ba2c4f2145b59c8778b5e4734ebf2bcfb60
ReidHillis <reidhillis7@gmail.com>
2025-09-18 21:06:46 -0400

    Update BrandBFactory.java

--- src/BrandBFactory.java
+++ src/BrandBFactory.java
@@ -4,13 +4,13 @@ public class BrandBFactory implements SmartDeviceFactory {
     @Override
     public SmartBulb createBulb() {
         SmartBulb bulb = new BrandBSmartBulb(); // Create new Brand B bulb
-        bulb.loadUsage("C:\\Users\\Drew\\OneDrive - Ontario Tech University\\Documents\\Software Architechture\\Assignment-1-Design-Patterns-main\\BrandBBulb.txt"); // Load data from external file
+        bulb.loadUsage("resources/BrandBBulb.txt"); // Load data from external file
         return bulb;
     }
     @Override
     public SmartLock createLock() {
         SmartLock lock = new BrandBSmartLock(); // Create new Brand B lock
-        lock.loadUsage("C:\\Users\\Drew\\OneDrive - Ontario Tech University\\Documents\\Software Architechture\\Assignment-1-Design-Patterns-main\\BrandBLock.txt"); // Load data from external file
+        lock.loadUsage("resources/BrandBLock.txt"); // Load data from external file
         return lock;
     }
 }
